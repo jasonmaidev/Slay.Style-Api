88ce7b501e682a74ea328b2dd4642506d5ce220f
JFMai90 <jasonmaif@gmail.com>
2023-10-16 00:00:19 -0700

    update styles page cache time

--- routes/styles.js
+++ routes/styles.js
@@ -17,11 +17,11 @@ let cache = apicache.middleware
 const router = express.Router()
 
 router.post("/:guestUser/:dailyAllowedSaves", verifyToken, createStyle)
-router.get("/:userId", verifyToken, cache("3 seconds"), getUserStyles)
-router.get("/style/:id", verifyToken, cache("3 seconds"), getStyle)
+router.get("/:userId", verifyToken, cache("5 seconds"), getUserStyles)
+router.get("/style/:id", verifyToken, cache("5 seconds"), getStyle)
 router.get("/:userId/count", verifyToken, getStylesCount)
 router.get("/:userId/pagecount", verifyToken, getStylesPageCount)
-router.get("/:userId/:sortByOccasion", verifyToken, cache("3 seconds"), getSuitableStyles)
+router.get("/:userId/:sortByOccasion", verifyToken, cache("5 seconds"), getSuitableStyles)
 router.get("/:userId/pagecount/:sortByOccasion", verifyToken, cache("2 seconds"), getSuitableStylesCount)
 
 router.patch("/:id/update/:guestUser/:dailyAllowedEdits", verifyToken, updateStyle)
